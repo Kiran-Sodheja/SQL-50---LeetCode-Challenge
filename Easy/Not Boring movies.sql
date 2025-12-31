727c583492e5ba2a9dbe627be558307f8ced188b
Kiran Sodheja <130165244+Kiran-Sodheja@users.noreply.github.com>
2025-12-31 18:01:22 +0530

    Update Not Boring movies.sql

--- Easy/Not Boring movies.sql
+++ Easy/Not Boring movies.sql
@@ -5,3 +5,10 @@ order by rating desc
 
 --OR
 Select id,movie,description,rating from Cinema where mod(id,2)=1 and description!="boring" order by rating desc;
+
+-- odd id, "boring", rating desc
+SELECT *
+FROM Cinema
+WHERE id % 2 <> 0 
+AND description <> "boring"
+ORDER BY rating DESC
